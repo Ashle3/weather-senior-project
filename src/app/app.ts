b4f4ba4c3b8a2fb6c58fdbc4a4d30d7c2da2cc27
Ashle3 <ashleeb3@byui.edu>
2025-06-20 21:46:31 -0500

    started mapping out current conditions component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ConditionsComponent } from './conditions/conditions.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [/*RouterOutlet,*/ HeaderComponent, ConditionsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/conditions/conditions.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-conditions',
+  imports: [],
+  templateUrl: './conditions.component.html',
+  styleUrl: './conditions.component.css'
+})
+export class ConditionsComponent {
+
+  
+  
+}
