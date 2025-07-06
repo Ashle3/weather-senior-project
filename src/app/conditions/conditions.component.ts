3e3313044e51a3ec15905589ac2ee09d125ea2c0
Ashle3 <ashleeb3@byui.edu>
2025-07-05 23:39:30 -0500

    got the current conditions to display, minus the icon

--- src/app/conditions/conditions.component.ts
+++ src/app/conditions/conditions.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-conditions',
-  imports: [],
+  imports: [DecimalPipe, DatePipe],
   templateUrl: './conditions.component.html',
-  styleUrl: './conditions.component.css'
+  styleUrl: './conditions.component.css',
+  standalone: true
 })
-export class ConditionsComponent {
+export class ConditionsComponent implements OnInit{
+  weatherData = signal<any>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
 
-  
-  
+  ngOnInit () {
+    const subscription = this.httpClient.get(
+      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/frisco%2C%20tx?unitGroup=us&include=days%2Ccurrent%2Calerts&key=UUYMN9MVZAQEYXMBE5XZKVRSH&contentType=json'
+    ).subscribe({
+      next: (resData) => {
+        console.log(resData);
+        this.weatherData.set(resData);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
